1ee6f02e99dc5fef191f7b0817fe41df239cdfac
Luuk Bruins <luukbruins060@hotmail.com>
2020-01-14 12:59:27 +0000

    application.py

--- application.py
+++ application.py
@@ -24,7 +24,6 @@ def after_request(response):
 
 
 # Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
