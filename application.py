048aa2915dd3f8c31da456808dbf1b506e3c5c47
Luuk Bruins <luukbruins2000@hotmail.com>
2020-01-23 14:05:43 +0000

    application.py

--- application.py
+++ application.py
@@ -263,7 +263,7 @@ def wacht():
 @app.route("/game", methods=["GET", "POST"])
 def startsinglegame():
     if request.method == "GET":
-        quest = newquestion()
+        quest = vragen()
         question = quest[0]
         coranswer = quest[1]
         answerlist = quest[2]
@@ -295,7 +295,7 @@ def startsinglegame():
 @app.route("/gamewcode", methods=["GET", "POST"])
 def gamewcode():
     if request.method == "GET":
-        quest = newquestion()
+        quest = vragen()
         question = quest[0]
         coranswer = quest[1]
         answerlist = quest[2]
@@ -335,7 +335,19 @@ def newquestion():
 
 def vragen():
     response = requests.get("https://opentdb.com/api.php?amount=49&category=21&type=multiple")
+    apis = {"sport":"https://opentdb.com/api.php?amount=49&category=21&type=multiple", "geography": "https://opentdb.com/api.php?amount=49&category=22&type=multiple", "history":"https://opentdb.com/api.php?amount=49&category=23&type=multiple", "animals": "https://opentdb.com/api.php?amount=49&category=27&type=multiple"}
+    api = random.choice(list(apis.keys()))
+    response = requests.get(apis[api])
     sport = response.json()
+    vragen = sport["results"]
+    sequence = random.choice(vragen)
+    category = sequence["category"]
+    question = sequence["question"]
+    coranswer = sequence["correct_answer"]
+    answerlist = sequence["incorrect_answers"]
+    answerlist.append(coranswer)
+    # print(coranswer)
+    return [question, coranswer, answerlist, category]
 
 @app.route("/eind", methods=["GET", "POST"])
 def eind():
@@ -412,7 +424,7 @@ def gamewfriend():
 @login_required
 def fspel():
     if request.method == "GET":
-        quest = newquestion()
+        quest = vragen()
         question = quest[0]
         coranswer = quest[1]
         answerlist = quest[2]
